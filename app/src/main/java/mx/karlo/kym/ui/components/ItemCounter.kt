a34c20eb82a383085b36b6e639bfc5caf7e22130
karlo-360 <karlod1130@gmail.com>
2026-03-12 21:22:55 -0700

    Update: ItemCounter for optional UI

--- app/src/main/java/mx/karlo/kym/ui/components/ItemCounter.kt
+++ app/src/main/java/mx/karlo/kym/ui/components/ItemCounter.kt
@@ -1,6 +1,11 @@
 package mx.karlo.kym.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
@@ -8,35 +13,59 @@ import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ItemCounter(
     count: Float = 0f,
+    showUI: Boolean = true,
     minusOnClick: () -> Unit,
     plusOnClick: () -> Unit,
     content: @Composable (Float) -> Unit
 ) {
 
     Row(
-        verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        FilledIconButton(
-            onClick = minusOnClick
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Remove,
-                contentDescription = "Reduce"
-            )
-        }
-        content(count)
 
-        FilledIconButton(
-            onClick = plusOnClick
+        AnimatedVisibility(
+            visible = showUI,
+            enter = fadeIn(),
+            exit = fadeOut()
         ) {
-            Icon(
-                imageVector = Icons.Filled.Add,
-                contentDescription = "Add"
-            )
+            FilledIconButton(
+                onClick = minusOnClick
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Remove,
+                    contentDescription = "Reduce"
+                )
+            }
         }
+
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            content(count)
+        }
+
+        AnimatedVisibility(
+            visible = showUI,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            FilledIconButton(
+                onClick = plusOnClick
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Add"
+                )
+            }
+        }
+
     }
 }
